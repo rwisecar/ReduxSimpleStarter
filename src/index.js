ffb85fe94eb71c0115cb860f2e4685c5e1c565f5
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-11-18 16:24:36 -0800

    setting up first nested component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const App = () => {
-  return <div>Hello world!</div>;
-}
+  return ( 
+    <div>
+    <SearchBar />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));
