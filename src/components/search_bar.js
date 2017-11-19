ce0e04a5bb96e3063ecba64a756ebd7e416d29b1
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-11-19 09:48:45 -0800

    refactoring to apply default state value to input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,14 +6,15 @@ class SearchBar extends Component {
   constructor (props) {
     super(props);
 
-    this.state = { term: ''};
+    this.state = { term: 'Search' };
   }
 
   render() {
     return (
       <div>
-        <input onChange={event => this.setState({ term: event.target.value })} />
-        Value of the input: {this.state.term}
+        <input 
+          value={ this.state.term }
+          onChange={event => this.setState({ term: event.target.value })} />
       </div>
     );
   }
